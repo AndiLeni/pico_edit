a058cae1ef07ef15fe4fc32ab5e80da6a7d21fd4
Andreas Lenhardt <andi.lenhardt@googlemail.com>
2017-06-23 16:08:50 +0200

    Removed password

--- config.php
+++ config.php
@@ -7,6 +7,6 @@ global $backend_password;
  * This should be a sha1 hash of your password.
  * Use a tool like http://www.sha1-online.com to generate.
  */
-$backend_password = 'd033e22ae348aeb5660fc2140aec35850c4da997';
+$backend_password = '';
 
 ?>
